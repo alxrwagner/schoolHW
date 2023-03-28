b973eac12b2d7098e54c6c5b9743cf7389b2c586
alxrw <alxr.wagner@gmail.com>
2023-03-28 16:07:26 +0700

    Completed

--- src/test/java/ru/hogwarts/schoolHW/controller/HouseControllerTest.java
+++ src/test/java/ru/hogwarts/schoolHW/controller/HouseControllerTest.java
@@ -9,11 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import ru.hogwarts.schoolHW.config.TestConfig;
 import ru.hogwarts.schoolHW.model.Faculty;
@@ -26,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @SpringBootTest
 @Testcontainers
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 class HouseControllerTest extends TestConfig {
 
     @Autowired
--- src/test/java/ru/hogwarts/schoolHW/controller/StudentControllerTest.java
+++ src/test/java/ru/hogwarts/schoolHW/controller/StudentControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import ru.hogwarts.schoolHW.config.TestConfig;
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @SpringBootTest
 @Testcontainers
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 class StudentControllerTest extends TestConfig {
 
     @Autowired
